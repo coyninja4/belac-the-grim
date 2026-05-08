c3b927cd1ce40ff523d46096032ca0b8c7f291aa
yoshca <Caleb.Yoshihara@wallawalla.edu>
2026-05-08 14:29:04 -0700

    starting cog setup

---
+++ DebugCog.py
@@ -0,0 +1,16 @@
+from discord import app_commands
+from discord.ext import commands
+import discord
+
+class MyCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def sync(self, ctx):
+        await ctx.send("Hello!")
+    
+    @commands.command()
+    async def test(ctx):
+        await ctx.send('🤏 pp')
+        print(f"{ctx.author}'s confidence shattered in {ctx.guild}, {ctx.channel}")
--- Main.py
+++ Main.py
@@ -1,76 +1,27 @@
 import os
+import json
 from dotenv import load_dotenv
 from discord.ext import commands
 import discord
 
-import RoleRandominterface
-
-RRI = RoleRandominterface
+import RoleRandomClass
 
 load_dotenv()
 
 BOT_KEY = os.getenv('BOT_KEY')
 
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
-intents.dm_messages = True
-intents.reactions = True
-global queue
+class Belac(commands.bot):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.message_content = True
+        intents.members = True
+        intents.dm_messages = True
+        intents.reactions = True
+        super.__init__(command_prefix='!', intents=intents)
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+    async def on_ready(self):
+        print(f'We have logged in as {self.user}')
+    async def setup_hook(self)
 
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}')
-
-@bot.command()
-async def test(ctx):
-    await ctx.send('🤏 pp')
-    print(f"{ctx.author}'s confidence shattered in {ctx.channel}")
-
-@bot.command()
-async def open_queue(ctx):
-    global queue
-    queue = set()
-    msg = await ctx.send(f"React \"✅\" to join. Remove reaction to leave queue.\nWhen ready type !game_start followed by the game.")
-    global tracked_message_id
-    tracked_message_id = msg.id
-    await msg.add_reaction("✅")
-
-@bot.command()
-async def game_start(ctx, *exclusions):
-    RRI.start_game(queue, exclusions)
-    print(f"game started with queue: {queue}")
-    queue = set()
-
-@bot.command()
-async def begone(ctx):
-    if ctx.author.id == 571322376491368477:
-        await ctx.send('booo 👎')
-        quit()
-    else:
-        await ctx.send(f'{ctx.author} is not worthy')
-
-@bot.event
-async def on_reaction_add(reaction, user):
-    global tracked_message_id
-    if reaction.message.id == tracked_message_id and user.id != 1488281701867065395:
-        if reaction.emoji == "✅":
-            queue.add(user.name)
-        else:
-            pass
-    print(queue)
-
-@bot.event
-async def on_reaction_remove(reaction, user):
-    global tracked_message_id
-    if reaction.message.id == tracked_message_id and user.id != 1488281701867065395:
-        if reaction.emoji == "✅":
-            try:
-                queue.remove(user.name)
-            except(KeyError):
-                pass
-    print(queue)
-
+bot = Belac()
 bot.run(BOT_KEY)
