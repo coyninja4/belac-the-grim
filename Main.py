75e78061ec0ef912a75870f806e9778ebe57f5c4
yoshca <Caleb.Yoshihara@wallawalla.edu>
2026-05-11 23:17:42 -0700

    fixed typos

--- Main.py
+++ Main.py
@@ -7,14 +7,14 @@ load_dotenv()
 
 BOT_KEY = os.getenv('BOT_KEY')
 
-class Belac(commands.bot):
+class Belac(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
         intents.members = True
         intents.dm_messages = True
         intents.reactions = True
-        super.__init__(command_prefix='!', intents=intents)
+        super().__init__(command_prefix='!', intents=intents)
 
     async def on_ready(self):
         print(f'We have logged in as {self.user}')
